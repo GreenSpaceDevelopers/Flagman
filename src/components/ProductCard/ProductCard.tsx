94167e1a5d1b5b57ac0285a3ec56e12f15a118e4
NaMol4anke <r.efremenko13@gmail.com>
2024-10-28 15:10:53 +0300

    Flagman-8 Done

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import MyModal from '../MyModal/MyModal';
+import './ProductCard.css';
+
+interface ProductCardProps {
+    imgUrl: string;
+    title: string;
+    desc: string;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ imgUrl, title, desc }) => {
+    const [productDescModalVisible, setProductDescModalVisible] = useState<boolean>(false);
+    
+    return (
+        <>
+            <div className='productCard'>
+                <img src={imgUrl} alt={title} />
+                <h5>{title}</h5>
+                <button onClick={() => setProductDescModalVisible(!productDescModalVisible)}>Подробнее</button>
+            </div>
+            
+            <MyModal visible={productDescModalVisible} setVisible={setProductDescModalVisible}>
+                <div className='productCard_modal_mainContainer'>
+                    <div className='productCard_modal_imgContainer'>
+                        <img src={imgUrl} alt={title} />
+                    </div>
+                    
+                    <div className='productCard_modal_descContainer'>
+                        <h2>{title}</h2>
+                        <p>
+                            {desc}
+                        </p>
+                    </div>
+                </div>
+            </MyModal>
+        </>
+    );
+}
+
+export default ProductCard;
