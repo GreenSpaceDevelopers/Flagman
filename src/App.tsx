28a219e31ca384097fa73078ab9620acda7e8961
NaMol4anke <r.efremenko13@gmail.com>
2024-10-30 14:40:09 +0300

    Scroll fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import AppRouter from './components/AppRouter';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import { ArrowUpSquareFill } from 'react-bootstrap-icons';
+import { useLocation } from 'react-router-dom';
 
 const App: React.FC = function() {
     const [navbarHeight, setNavbarHeight] = useState<number>(0);
     const navbarRef = useRef<HTMLElement | null>(null);
     const screenWidth = window.screen.width;
+    const location = useLocation();
     
     useEffect(() => {
         if (navbarRef.current) {
             setNavbarHeight(navbarRef.current.offsetHeight);
         }
     }, []); 
+    
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
     
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
