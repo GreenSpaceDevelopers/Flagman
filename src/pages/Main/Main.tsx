68ff51d6ff21f0e228420e401d536ceef88f1819
NaMol4anke <r.efremenko13@gmail.comgit config --global user.email r.efremenko13@gmail.com>
2025-02-03 15:40:55 +0700

    Add Certificate

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,6 +4,7 @@ import './Main.css';
 import useIntersectionObserver from '../../Hooks/useIntersectionObserver';
 import SimpleSlider from '../../components/SimpleSlider/SimpleSlider';
 import {useEffect, useState} from "react";
+import FlagmanCertificate from '../../assets/images/FlagmanCertificate.png';
 
 const Main: React.FC = () => {
     const [isPageVisible, setPageVisible] = useState<boolean>(false);
@@ -21,20 +22,20 @@ const Main: React.FC = () => {
                 </header>
 
                 <main>
-                    {/*<div className='main_companyInfo_container'>*/}
-                    {/*    <div className='main_companyInfo_text'>*/}
-                    {/*        <h2>О компании</h2>*/}
-                    {/*        <p>*/}
-                    {/*            ООО "Флагман" — официальный дилер АО "Опытный завод "Микрон". Проектирование, поставка, собственное производство, */}
-                    {/*            монтаж торгового и складского стеллажного оборудования. Все виды стеллажей, комплектующие, аксессуары, нестандартное */}
-                    {/*            оборудование под заказ. Мы всегда готовы реализовывать вместе с вами самые смелые и амбициозные проекты.*/}
-                    {/*        </p>*/}
-                    {/*    </div>*/}
-                    {/*    */}
-                    {/*    <div className='main_companyInfo_imgContainer'>*/}
-                    {/*        <img className='main_companyInfo_img' alt='Flagman' />*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    <div className='main_companyInfo_container'>
+                        <div className='main_companyInfo_text'>
+                            <h2>О компании</h2>
+                            <p>
+                                ООО "Флагман" — официальный дилер АО "Опытный завод "Микрон". Проектирование, поставка, собственное производство, 
+                                монтаж торгового и складского стеллажного оборудования. Все виды стеллажей, комплектующие, аксессуары, нестандартное 
+                                оборудование под заказ. Мы всегда готовы реализовывать вместе с вами самые смелые и амбициозные проекты.
+                            </p>
+                        </div>
+                        
+                        <div className='main_companyInfo_imgContainer'>
+                            <img className='main_companyInfo_img' src={FlagmanCertificate} alt='Flagman' />
+                        </div>
+                    </div>
 
                     <div className='main_photogallery_container'>
                         <PhotoGallery/>
